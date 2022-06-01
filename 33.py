d96784acb6277bc9f1b727e35be73b83e426ba86
C110156234 <102135334+C110156234@users.noreply.github.com>
2022-06-01 13:53:22 +0800

    Add files via upload

--- 33.py
+++ 33.py
@@ -4,7 +4,6 @@ m = int(input("微積分："))
 p = int(input("體育："))
 d = int(input("程式設計："))
 avg1 = (c+e+m+p+d) / 5
-print("平均分數為%.2f" %(avg1))
 list1 = [c,e,m,p,d]
 max_value = None
 for i in list1:
@@ -14,6 +13,6 @@ min_value = None
 for i in list1:
     if (min_value is None or i < min_value):
         min_value = i
-print("平均分數:",avg1)
+print("平均分數為%.2f" %(avg1))
 print("最高分科目:",max_value,"分")
 print("最低分科目:",min_value,"分")
