a70e6ea723ee850f3dec5068179c0d1dd9a83dc9
C110156234 <102135334+C110156234@users.noreply.github.com>
2022-06-01 22:56:16 +0800

    Add files via upload

--- 33.py
+++ 33.py
@@ -3,16 +3,18 @@ e = int(input("英文："))
 m = int(input("微積分："))
 p = int(input("體育："))
 d = int(input("程式設計："))
+dict1 = {c:"國文",e:"英文",m:"微積分",p:"體育",d:"程式設計"}
 avg1 = (c+e+m+p+d) / 5
 list1 = [c,e,m,p,d]
-max_value = None
-for i in list1:
-    if (max_value is None or i > max_value):
-        max_value = i
-min_value = None
-for i in list1:
-    if (min_value is None or i < min_value):
-        min_value = i
+x = 0
+for i in range(len(list1)):
+    for j in range(len(list1)-1-i):
+        if list1[j]<list1[j+1]:
+            x = list1[j]
+            list1[j]=list1[j+1]
+            list1[j+1]=x
+max_value = list1[0]
+min_value = list1[4]
 print("平均分數為%.2f" %(avg1))
-print("最高分科目:",max_value,"分")
-print("最低分科目:",min_value,"分")
+print("最高分科目:%s%d分" %(dict1[max_value],max_value))
+print("最低分科目:%s%d分" %(dict1[min_value],min_value))
