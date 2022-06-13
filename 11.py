5f00297dd276e5647299855b35acc16daa301257
C110156234 <102135334+C110156234@users.noreply.github.com>
2022-06-13 22:24:51 +0800

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,49 @@
+m,d = input("輸入月及日為：").split();m = int(m);d = int(d)
+if m == 1 and d <= 20:
+    print("星座為：Apricorn")
+if m == 1 and 21 <= d <= 31:
+    print("星座為：Aquarius")
+if m == 2 and d <= 18:
+    print("星座為：Aquarius")
+if m == 2 and 19 <= d <= 29:
+    print("星座為：Pisces")
+if m == 3 and d <= 20:
+    print("星座為：Pisces")
+if m == 3 and 21 <= d <= 31:
+    print("星座為：Aries")
+if m == 4 and d <= 20:
+    print("星座為：Aries")
+if m == 4 and 21 <= d <= 30:
+    print("星座為：Taurus")
+if m == 5 and d <= 21:
+    print("星座為：Aries")
+if m == 5 and 22 <= d <= 31:
+    print("星座為：Gemini")
+if m == 6 and d <= 21:
+    print("星座為：Gemini")
+if m == 6 and 22 <= d <= 30:
+    print("星座為：Cancer")
+if m == 7 and d <= 22:
+    print("星座為：Cancer")
+if m == 7 and 23 <= d <= 31:
+    print("星座為：Leo")
+if m == 8 and d <= 23:
+    print("星座為：Leo")
+if m == 8 and 24 <= d <= 31:
+    print("星座為：Virgo")
+if m == 9 and d <= 23:
+    print("星座為：Virgo")
+if m == 9 and 24 <= d <= 30:
+    print("星座為：Libra")
+if m == 10 and d <= 23:
+    print("星座為：Libra")
+if m == 10 and 24 <= d <= 31:
+    print("星座為：Scorpio")
+if m == 11 and d <= 22:
+    print("星座為：Scorpio")
+if m == 11 and 23 <= d <= 30:
+    print("星座為：Sagittarius")
+if m == 12 and d <= 22:
+    print("星座為：Sagittarius")
+if m == 12 and 23 <= d <= 31:
+    print("星座為：Apricorn")
